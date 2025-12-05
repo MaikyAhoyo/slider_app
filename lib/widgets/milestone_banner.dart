fabcc89b00452fd5c7852fdc0390acbef26227dd
Maiky-ahoyo <arroyomiguel590@gmail.com>
2025-12-05 16:00:48 -0700

    nuevos banners

--- lib/widgets/milestone_banner.dart
+++ lib/widgets/milestone_banner.dart
@@ -19,27 +19,45 @@ class _MilestoneBannerState extends State<MilestoneBanner>
   late AnimationController _controller;
   late Animation<double> _scaleAnimation;
   late Animation<double> _opacityAnimation;
+  late Animation<double> _tiltAnimation;
 
   @override
   void initState() {
     super.initState();
     _controller = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 2000),
+      duration: const Duration(milliseconds: 2500),
     );
 
     _scaleAnimation = TweenSequence<double>([
-      TweenSequenceItem(tween: Tween(begin: 0.0, end: 1.2), weight: 20),
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 0.0,
+          end: 1.2,
+        ).chain(CurveTween(curve: Curves.easeOutBack)),
+        weight: 20,
+      ),
       TweenSequenceItem(tween: Tween(begin: 1.2, end: 1.0), weight: 10),
-      TweenSequenceItem(tween: ConstantTween(1.0), weight: 50),
-      TweenSequenceItem(tween: Tween(begin: 1.0, end: 0.0), weight: 20),
+      TweenSequenceItem(tween: ConstantTween(1.0), weight: 60),
+      TweenSequenceItem(
+        tween: Tween(
+          begin: 1.0,
+          end: 0.0,
+        ).chain(CurveTween(curve: Curves.easeInBack)),
+        weight: 10,
+      ),
     ]).animate(_controller);
 
     _opacityAnimation = TweenSequence<double>([
-      TweenSequenceItem(tween: Tween(begin: 0.0, end: 1.0), weight: 20),
-      TweenSequenceItem(tween: ConstantTween(1.0), weight: 60),
-      TweenSequenceItem(tween: Tween(begin: 1.0, end: 0.0), weight: 20),
+      TweenSequenceItem(tween: Tween(begin: 0.0, end: 1.0), weight: 10),
+      TweenSequenceItem(tween: ConstantTween(1.0), weight: 80),
+      TweenSequenceItem(tween: Tween(begin: 1.0, end: 0.0), weight: 10),
     ]).animate(_controller);
+
+    _tiltAnimation = TweenSequence<double>([
+      TweenSequenceItem(tween: Tween(begin: -0.05, end: 0.05), weight: 50),
+      TweenSequenceItem(tween: Tween(begin: 0.05, end: 0.0), weight: 50),
+    ]).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
 
     _controller.forward().then((_) => widget.onAnimationComplete());
   }
@@ -59,35 +77,52 @@ class _MilestoneBannerState extends State<MilestoneBanner>
           opacity: _opacityAnimation.value,
           child: Transform.scale(
             scale: _scaleAnimation.value,
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [Colors.purple, Colors.blueAccent],
+            child: Transform.rotate(
+              angle: _tiltAnimation.value,
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 24,
+                  vertical: 12,
                 ),
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(color: Colors.white, width: 3),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.black45,
-                    blurRadius: 10,
-                    offset: Offset(0, 5),
-                  )
-                ],
-              ),
-              child: Text(
-                widget.text,
-                style: const TextStyle(
-                  fontFamily: 'Courier',
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.yellowAccent,
-                  shadows: [
-                    Shadow(
-                      blurRadius: 2,
+                decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                    colors: [Color(0xFF0000AA), Color(0xFF000044)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                  border: Border.all(color: Colors.white, width: 3),
+                  boxShadow: const [
+                    BoxShadow(
                       color: Colors.black,
-                      offset: Offset(2, 2),
-                    )
+                      blurRadius: 0,
+                      offset: Offset(6, 6),
+                    ),
+                  ],
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Icon(Icons.flag, color: Colors.greenAccent, size: 28),
+                    const SizedBox(width: 12),
+
+                    Text(
+                      widget.text.toUpperCase(),
+                      style: const TextStyle(
+                        fontFamily: 'Courier',
+                        fontSize: 22,
+                        fontWeight: FontWeight.w900,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white,
+                        letterSpacing: 2.0,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 0,
+                            color: Colors.black,
+                            offset: Offset(2, 2),
+                          ),
+                        ],
+                      ),
+                    ),
                   ],
                 ),
               ),
